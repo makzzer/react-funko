1ccb16f543e4bc3da7e8b264a688eceb7d71ab97
Makz Sánchez <makzofx@gmail.com>
2023-05-18 08:05:28 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ const App = () => {
           <h2 className="text-white text-2xl mb-2 px-auto">
             Descubrí el proximo Funko de tu colección
           </h2>
-          <button className="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-4 rounded-full">
+          <button className="text-lg mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-4 rounded-full">
             Comprar{" "}
           </button>
         </div>
@@ -76,7 +76,7 @@ const App = () => {
               colección.
             </p>
             <div className="border border-red-400 mb-2"> </div>
-            <button className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-full">Ver colección</button>
+            <button className="px-8 py-4 text-lg bg-gray-800 hover:bg-gray-900 text-white font-bold rounded-full">Ver colección</button>
           </div>
 
           <div>
