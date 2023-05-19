636c9622623180cd8faed1f9c19a4775ed45f0b5
Maximiliano Sanchez <makzofx@gmail.com>
2023-05-19 00:07:13 -0300

    test mobile

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,25 @@ const elementosLanding = [
   { id: 1, title: 'STARS WARS & THE MANDALORIAN', img: 'src/assets/images/yoda.webp', description: 'Disfruta de una saga que sigue agregando personajes a su colección.' },
   { id: 2, title: 'POKEMÓN INDIGO', img: 'src/assets/images/charmander-1.webp', description: 'Atrapa todos los que puedas y disfruta de una colección llena de amigos.' },
   { id: 3, title: 'HARRY POTTER', img: 'src/assets/images/harry-1.webp', description: 'Revive los recuerdos de una saga llena de magia y encanto.' },
-]
+];
 
 const App = () => {
   const [elementosLand, SetElem] = useState(elementosLanding);
 
   return (
+    <div className="container mx-auto">
+      <NavBar />
 
-    <>
-
-      <header>
-
-
-
-        <div className="h-[50vh] bg-fixed bg-no-repeat bg-cover bg-center bg-[url('./assets/images/funkos-banner.webp')]">
-          <NavBar />
-          <div className="container mx-auto h-full">
-            <Branding />
-          </div>
+      <div className="h-[50vh] bg-fixed bg-no-repeat bg-cover bg-center bg-[url('./assets/images/funkos-banner.webp')]">
+        <div className="container mx-auto h-full">
+          <Branding />
         </div>
-      </header>
+      </div>
 
-
-
-      <main className="container mx-auto">
+      <main>
         <ListaItemsTienda elementosLand={elementosLand} />
       </main>
-
-
-    </>
+    </div>
   );
 };
 
