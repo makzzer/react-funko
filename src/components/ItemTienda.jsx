3760bd8fb9d8cc6ed07e55586c0ca2ebe55ac15a
Maximiliano Sanchez <makzofx@gmail.com>
2023-05-22 17:24:08 -0300

    pequeñas correcciones

--- src/components/ItemTienda.jsx
+++ src/components/ItemTienda.jsx
@@ -4,9 +4,9 @@ const ItemTienda = ({ id, title, description, img }) => {
 
   return (
     <>
-      <div className=" flex flex-col mb-1 mx-auto px-auto justify-around  container md:flex-row max-w-screen-2xl">
+      <div className=" flex flex-col items-center mb-1 mx-auto px-auto container md:flex-row max-w-screen-2xl">
 
-        <article className="flex flex-col md:text-start text-center  mx-auto justify-center">
+        <article className="flex flex-col md:text-start text-center mx-auto ">
 
           <h3 className="text-gray-800 text-4xl mb-4 whitespace-pre-line">
             {title}
@@ -14,7 +14,7 @@ const ItemTienda = ({ id, title, description, img }) => {
           <p className="text-gray-800 text-2xl mb-4">
             {description}
           </p>
-          <div className="border border-red-400 mb-4"> </div>
+          <div className="border-2 border-red-500 mb-4 max-w-xl mx-4 md:mx-0"> </div>
           <div className="flex justify-center md:justify-start">
             <button className="px-20 py-4 text-2xl bg-gray-800 hover:bg-gray-900 text-white font-bold rounded-full">Ver colección</button>
           </div>
