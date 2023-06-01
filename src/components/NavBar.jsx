a7b954aab43289e7aee1b6cc1fb071ac8fe2b077
Maximiliano <makzofx@gmail.com>
2023-06-01 14:54:47 -0300

    otra version

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -54,7 +54,7 @@ const Nav = () => {
   return (
     <>
       {/**creo el contendor principal donde coloco fixed para que la barra quede pegada cuando scrolleo"*/}
-      <div className="bg-gray-900 shadow-md w-full fixed top-0 left-0 md:py-4 py-0 ">
+      <div className="bg-gray-900 shadow-md w-full fixed top-0 left-0 md:py-4 py-2 md:pb-0 ">
         {/**creo otro div  que va a contener por un lado al logo, al boton para ampliar y a los links"*/}
         <div className="md:py-1 mx-auto ps-6 md:flex items-center container justify-between  bg-gray-900  md:px-10 px-2 ">
           {/**Div con el logo  */}
@@ -74,27 +74,24 @@ const Nav = () => {
 
           <div
             onClick={() => setOpen(!open)}
-            className="text-3xl absolute right-4 top-8 cursor-pointer ps-2 md:hidden"
+            className="text-3xl absolute right-4 top-10 cursor-pointer ps-2 md:hidden"
           >
             <ion-icon name={open ? "close" : "menu"}></ion-icon>
+            
           </div>
 
-          {open
-          ? <hr className="border-1 min-w-full border-red-500 mb-2 mt-0" />
-          : <hr className="hidden transition-all border-1 min-w-full border-red-500 mb-2 mt-0" />
 
-          }
 
             
           <ul
-            className={`md:flex md:items-center md:pb-0  absolute md:static bg-gray-900 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            className={`md:flex md:items-center md:pb-0  absolute md:static pb-1 bg-gray-900 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
               open ? "top-20 " : "top-[-490px]"
             }`}
           >
             {navlinks.map((navlink) => {
               return (
                 <>
-                  <li key={navlink.name} className="text-xl md:my-0 my-7">
+                  <li key={navlink.name} className="bg-gray-900 text-xl md:my-0 my-7">
                     <NavLink to={navlink.to} className={navlink.classname}>
                       {navlink.name}
                     </NavLink>
