3d23f824c09ac1e0b102969b1d20bc285b512d83
Maximiliano <makzofx@gmail.com>
2023-06-07 03:38:15 -0300

    pie de pagina mejorado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,67 +2,36 @@ import IconLogotipoSimple from "./icons/IconLogotipoSimple"
 
 const Footer = () => {
     return (
-        <div className="bg-gray-900 shadow-md relative mx-auto py-2 md:pb-4 bottom-0 left-0 w-full h-full">
+        //container general
+        <div className="bg-gray-900 shadow-md relative mx-auto pt-1 md:pt-4 pb-0 md:py-2 md:pb-4 bottom-0 w-full left-0">
 
 
-        <footer className="bg-gray-900 py-2 px-4 text-white container max-w-6xl mx-auto">
+        <footer className="bg-gray-900 pb-2 justify-center items-center px-20 text-white container max-w-6xl mx-auto flex flex-col md:flex-row">
 
             {/*div con 2 elem: navbar y logo*/}
-            <div className="flex flex-row container mx-auto max-w-screen-2xl py-2">
+            <div className="flex flex-row container items-center justify-between  mx-auto max-w-screen-2xl py-2">
 
                 {/*div con navbar*/}
-                <div className="flex justify-start">
-                    <nav className="py-4 px-8 flex flex-col md:flex-row justify-between">
-
-
-
-                        <a
-                            href=""
-                            className="text-gray-300 px-3 py-2 rounded-md text-xl font-medium transition hover:-translate-y-1 hover:scale-110 hover:bg-red-500 duration-300"
-                        >
-                            Shop
-                        </a>
-
-
-
-                        <a
-                            href=""
-                            className="text-gray-300 px-3 py-2 rounded-md text-xl font-medium transition hover:-translate-y-1 hover:scale-110 hover:bg-red-500 duration-300"
-                        >
-                            Registrarse
-                        </a>
-
-
-
-                        <a
-                            href=""
-                            className="text-gray-300 px-3 py-2 rounded-md text-xl font-medium transition hover:-translate-y-1 hover:scale-110 hover:bg-red-500 duration-300"
-                        >
-                            Contacto
-                        </a>
-                        <a
-                            href=""
-                            className="text-gray-300 px-3 py-2 rounded-md text-xl font-medium transition hover:-translate-y-1 hover:scale-110 hover:bg-red-500 duration-300"
-                        >
-                            Ingresar
-                        </a>
-
-
-
-                    </nav>
-                </div>
-
+                <div className="flex justify-between gap-4 mb-1 md:mb-0">
+                <ion-icon name={"logo-instagram"} size="large"></ion-icon>
+                <ion-icon name="logo-facebook" size="large"></ion-icon>
+                <ion-icon name="logo-twitter" size="large"></ion-icon>
+                <ion-icon name="logo-twitch" size="large"></ion-icon>
 
                     {/*div con img logo*/}
                     <div className="flex m-auto md:m-0 md:ml-auto">
-                        <div className=" my-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-none duration-300"><IconLogotipoSimple ancho={40} alto={80} /></div>
+                        <div className=" my-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-none duration-300"><IconLogotipoSimple ancho={40} alto={28} /></div>
                     </div>
+                </div>
+
+
+
                 </div>
         
 
             {/**disclamer */}
             <div className="container mx-auto text-center">
-                <p>&copy; 2023 Funko! Todos los derechos reservados.</p>
+                <p>&copy; 2023 Funko Todos los derechos reservados.</p>
             </div>
 
         </footer>
