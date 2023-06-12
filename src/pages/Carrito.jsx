8b884fb3cfc1bedd975b2505e427b7f5a53e4c45
Maximiliano <makzofx@gmail.com>
2023-06-11 21:39:20 -0300

    mejor carrito scroll top button vaciar

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -2,21 +2,16 @@ import { useCarritoContext } from "../context/CarritoContext";
 import ElemCarrito from "../components/ElemCarrito";
 import { useEffect } from "react";
 
-
 //desestructuro el carrito
 const Carrito = () => {
   const { carrito, vaciarCarrito } = useCarritoContext();
-
 
   useEffect(() => {
     window.scrollTo(0, 0); // Hacer scroll al principio de la página al cargar el componente
   }, []);
 
-
-
   return (
     <>
-    
       <div className="min-h-screen w-full text-center items-center flex-col mt-6 md:m-20">
         <h1 className="pt-20 text-4xl md:text-6xl pb-4">Carrito</h1>
 
@@ -39,15 +34,16 @@ const Carrito = () => {
           <div className="items-center flex justify-center">
             <button
               className="py-2 bg-red-700 rounded-md px-2 m-2 text-md font-semibold text-white"
-              onClick={vaciarCarrito}
+              onClick={() => {
+                vaciarCarrito();
+                window.scrollTo(0, 0);
+              }}
             >
-              {" "}
               Vaciar Carrito{" "}
             </button>
           </div>
         )}
       </div>
-
     </>
   );
 };
