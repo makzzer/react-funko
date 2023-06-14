fab3ff2b9e48f4122020771a1f3fbee08c2b19bf
Maximiliano <makzofx@gmail.com>
2023-06-14 02:17:59 -0300

    carrito mas estilizado v1

--- src/components/ElemCarrito.jsx
+++ src/components/ElemCarrito.jsx
@@ -3,42 +3,46 @@ import { useCarritoContext } from "../context/CarritoContext";
 const ElemCarrito = ({ id, title, precio, img, cantidad }) => {
   const { eliminarElementoCarrito } = useCarritoContext();
 
-  //metodo que uso para eliminar el elemento metodo original desde carrito context
-  const eliminarElemento = (e) =>{
-    e.preventDefault()
-    eliminarElementoCarrito(id)
+  const eliminarElemento = (e) => {
+    e.preventDefault();
+    eliminarElementoCarrito(id);
     console.log(id);
-
-  }
-
-
+  };
 
   return (
-    <form className="uppercase max-w-md md:ms-0 md:max-w-none justify-start rounded-md md:p-4 m-2 flex md:flex-row mx-auto flex-col w-full">
-      <div className="flex md:flex-row flex-col md:justify-between  font-semibold items-center justify-center">
-
-
-        <button
-          className="ml-auto md:ml-0 z-0 cursor-pointer me-2"
-          onClick={eliminarElemento}
-        >
-          <ion-icon name="close-circle-outline" size="large"></ion-icon>
-        </button>
-
-        <div className="mx-auto items-center ">
-          <img src={img} alt="Producto" className="w-20 h-20" />
+    <div className="border-b border-gray-300">
+      <form className="uppercase max-w-md md:ms-0 md:max-w-none rounded-md md:p-4 m-2 flex md:flex-row justify-between mx-auto flex-col w-full">
+        <div className="flex md:flex-row flex-col md:justify-between font-semibold items-center justify-center">
+          <button
+            className="ml-auto md:ml-0 z-0 cursor-pointer me-2"
+            onClick={eliminarElemento}
+          >
+            <ion-icon name="close-circle-outline" size="large"></ion-icon>
+          </button>
+          <div className="mx-auto items-center md:ps-10">
+            <img src={img} alt="Producto" className="w-20 h-20" />
+          </div>
+          <div className="min-w-[16rem]">
+            <h1 className="items-start">Producto: {title}</h1>
+          </div>
+          <div>
+            <label htmlFor="cantidad" className="text-red-600 pr-2">
+              Cantidad:
+            </label>
+            <input
+              type="number"
+              id="cantidad"
+              value={cantidad}
+              className="w-7 text-center px-2 py-1 border border-gray-300 rounded-md focus:outline-none focus:border-red-600"
+              readOnly
+            />
+          </div>
+          <div>
+            <h1>Precio: ${precio}</h1>
+          </div>
         </div>
-        <div className="min-w-[16rem] ">
-          <h1 className="items-start">Producto: {title}</h1>
-        </div>
-        <div>
-          <h1 className="text-red-600 pe-2">Cantidad: {cantidad}</h1>
-        </div>
-        <div>
-          <h1>Precio: ${precio}</h1>
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
