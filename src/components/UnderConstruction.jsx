590749730122e0053b14cbb6d67b1ca194213164
Maximiliano <makzofx@gmail.com>
2023-06-14 00:05:53 -0300

    mejorados mensajes notf

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -1,21 +1,25 @@
+import { NavLink } from "react-router-dom";
 import IconoLogotipoSimple from "../components/icons/IconLogotipoSimple";
 
-const UnderConstruction = () => {
+const UnderConstruction = ({ mensaje }) => {
   return (
     <>
-      
-        <div className="mx-5 flex md:flex-row flex-col justify-center gap-6 items-center md:mx-0 container font-mono font-bold bg-gray-900 text-5xl mt-20 pb-10 pt-12">
-          <h1 className="text-white">Page under Construction</h1>
-          <div className="text-green-500">
-            {" "}
-            {/* Agrega la clase "text-green-500" */}
-            <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-none duration-300">
-              <IconoLogotipoSimple ancho={60} alto={80} />{" "}
-              {/* Puedes usar "#00FF00" para verde */}
-            </div>
-          </div>
+      <div className="mx-5 flex md:flex-row flex-col justify-center gap-6 items-center md:mx-0 mx-2 container font-mono font-bold bg-gray-900 text-5xl mt-20 pb-10 pt-12">
+        <h1 className="text-white">{mensaje}</h1>
+
+        <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-none duration-300">
+          <IconoLogotipoSimple ancho={60} alto={60} />
         </div>
-      
+
+        <div className="flex">
+          <NavLink
+            to={"/"}
+            className="m-2 p-2 bg-red-700 rounded-lg text-lg hover:bg-red-600 text-white"
+          >
+            Volver
+          </NavLink>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -2,11 +2,14 @@
 import Underconstruction from "../components/UnderConstruction"
 
 const Contacto = () => {
+  
+  const mensaje="Page under construction"
+  
   return (
     <>
       <div className="flex container mx-auto min-h-screen items-start m-20 text-center">
        
-        <Underconstruction/>
+        <Underconstruction mensaje={mensaje}/>
 
       </div>
     </>
