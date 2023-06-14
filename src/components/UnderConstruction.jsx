2590ad0cbba027cec7c6af95d011f541adbf1fc1
Maximiliano <makzofx@gmail.com>
2023-06-14 00:14:49 -0300

    arreglado under c responsive

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -4,7 +4,7 @@ import IconoLogotipoSimple from "../components/icons/IconLogotipoSimple";
 const UnderConstruction = ({ mensaje }) => {
   return (
     <>
-      <div className="mx-5 flex md:flex-row flex-col justify-center gap-6 items-center md:mx-0 mx-2 container font-mono font-bold bg-gray-900 text-5xl mt-20 pb-10 pt-12">
+      <div className=" flex md:flex-row flex-col justify-center gap-6 items-center md:mx-0 mx-2 container font-mono font-bold bg-gray-900 text-5xl mt-20 pb-10 pt-12">
         <h1 className="text-white">{mensaje}</h1>
 
         <div className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-none duration-300">
