c2479f8ed92769bbcdd7ef0498f6b164b1b5d97e
Maximiliano <makzofx@gmail.com>
2023-06-22 04:17:35 -0300

    modificacion temporal sin variables de entorno

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,9 +5,11 @@ import { getAuth,signInWithEmailAndPassword,createUserWithEmailAndPassword,signO
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+//console.log(import.meta.env.VITE_FIREBASE_PROJECT_ID)
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -16,6 +18,22 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   //measurementId: "G-P1ZZ8W6Z7V"
 };
+*/
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAdaEsnNsbMePv_ZvWN4pQAuHIpbzFgYGQ",
+    authDomain: "react-funko-819d3.firebaseapp.com",
+    projectId: "react-funko-819d3",
+    storageBucket: "react-funko-819d3.appspot.com",
+    messagingSenderId: "650833728217",
+    appId: "1:650833728217:web:032efaf9a832cd942c64c8",
+    measurementId: "G-P1ZZ8W6Z7V"
+  };
+
+
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
