799144bc5843fec252d986440129fd71752c5b74
Makz Sánchez <makzofx@gmail.com>
2023-06-23 02:05:44 -0300

    chau chiste manzi

--- src/pages/DashboardUser.jsx
+++ src/pages/DashboardUser.jsx
@@ -2,18 +2,14 @@ import Manzanita from "../components/Manzanita";
 import { useUserContext } from "../context/UserContext";
 
 const DashboardUser = () => {
+  const { user } = useUserContext();
 
-  const {user} = useUserContext()
-
-  //const mensaje = "Hola🙂    "+user.email;
-  const mensaje = "Hola Mica, me encanta cogerte toda la manzanita";
-
-  const mensaje2 = "🍎🖤"
+  const mensaje = "Hola🙂    " + user.email;
 
   return (
     <>
       <div className="flex container mx-auto min-h-screen items-start m-20 text-center">
-        <Manzanita mensaje={mensaje} mensaje2={mensaje2}/>
+        <Manzanita mensaje={mensaje} />
       </div>
     </>
   );
