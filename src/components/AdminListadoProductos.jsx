74e67d010ea2a9e4e5667e68af574703da31346a
Maximiliano <makzofx@gmail.com>
2023-06-23 22:47:42 -0300

    espectacular crear nuevo

--- src/components/AdminListadoProductos.jsx
+++ src/components/AdminListadoProductos.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-
-const AdminListadoProductos = ({grillaShop}) => {
+const AdminListadoProductos = ({ grillaShop }) => {
   const customers = [
     {
       id: 1,
@@ -21,46 +21,100 @@ const AdminListadoProductos = ({grillaShop}) => {
     },
   ];
 
+  const [elementos, setElementos] = useState(grillaShop);
 
-  const [elementos,setElementos] = useState(grillaShop)
-
-
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <>
-      <div className="container mx-auto">
+
+      <div className="container mx-auto flex flex-col">
+       
+       
+       
+       
+       
+        <div className="flex gap-2 justify-between flex-col md:flex-row items-center mb-10">
+          <input
+            type="text"
+            placeholder="Buscar productos"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className=" m-2 md:m-0 border w-full border-gray-300 px-4 py-2 rounded-lg "
+          />
+          <NavLink className="bg-red-700 inline-block md:py-2 flex-shrink-0 md:px-4 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+            Crear Nuevo
+          </NavLink>
+        </div>
+
         <div className="overflow-x-auto">
-          <table className="w-full">
-            <thead>
-              <tr>
-                <th className="border p-2">Imagen</th>
-                <th className="border p-2">ID</th>
-                <th className="border p-2">CÓDIGO</th>
-                <th className="border p-2">NOMBRE DEL PRODUCTO</th>
-                <th className="border p-2">CATEGORÍA</th>
-               
+
+       
+
+        <table className="w-full divide-y divide-gray-200 overflow-x-auto">
+          <thead>
+            <tr>
+              <th className="px-6 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Imagen
+              </th>
+              <th className="px-6 py-3 bg-gray-50 text-center  text-xs font-medium text-gray-500 uppercase tracking-wider">
+                ID
+              </th>
+              <th className="px-6 py-3 bg-gray-50 text-center  text-xs font-medium text-gray-500 uppercase tracking-wider">
+                CÓDIGO
+              </th>
+              <th className="px-6 py-3 bg-gray-50 text-center  text-xs font-medium text-gray-500 uppercase tracking-wider">
+                NOMBRE DEL PRODUCTO
+              </th>
+              <th className="px-6 py-3 bg-gray-50 text-center  text-xs font-medium text-gray-500 uppercase tracking-wider">
+                CATEGORÍA
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className="divide-y">
+
+            {elementos.map((elem) => (
+              <tr key={elem.id}>
+
+
+                <td className="px-6 py-4 whitespace-nowrap flex mx-auto justify-center">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0 h-10 w-10 ">
+                      <img
+                        src={elem.img}
+                        alt="Imagen del producto"
+                        className="h-12 w-12 mx-auto"
+                      />
+                    </div>
+                  </div>
+                </td>
+
+                <td className="px-6 py-4 whitespace-nowrap text-center">{elem.id}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-center">{elem.id}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-center">{elem.title}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-center">{elem.subTitle}</td>
+                <td className="px-6 py-4 whitespace-nowrap ">
+                  {" "}
+
+
+                  <button className="text-gray-900">
+                    <ion-icon name="create-outline"></ion-icon>
+                  </button>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {" "}
+                  <button className="text-red-700">
+                    <ion-icon name="trash-outline"></ion-icon>
+                  </button>
+                </td>
               </tr>
-            </thead>
-            <tbody className="font-semibold uppercase">
-              {elementos.map((elem) => (
-                <tr key={elem.id}>
-                  <td className="border p-2">
-                    <img
-                      src={elem.img}
-                      alt="Imagen del producto"
-                      className="h-10 w-10 mx-auto"
-                    />
-                  </td>
-                  <td className="border p-2">{elem.id}</td>
-                  <td className="border p-2">{elem.id}</td>
-                  <td className="border p-2">{elem.title}</td>
-                  <td className="border p-2">{elem.subTitle}</td>
-                  <td className="border p-2"> <button className="text-gray-900"><ion-icon name="create-outline"></ion-icon></button></td>
-                  <td className="border p-2"> <button className="text-red-700"><ion-icon name="trash-outline"></ion-icon></button></td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+            ))}
+
+
+          </tbody>
+        </table>
+
         </div>
       </div>
     </>
--- src/pages/DashboardUser.jsx
+++ src/pages/DashboardUser.jsx
@@ -32,22 +32,8 @@ const DashboardUser = () => {
 
   return (
     <>
-      <div className="transition-all duration-700 min-h-screen mx-auto max-w-6xl text-center items-center flex-col md:mt-20 mt-10">
+      <div className="transition-all duration-700 min-h-screen mx-auto max-w-6xl text-center items-center flex-col md:mt-20 mt-4 pt-20">
      
-        <div className="flex md:justify-between md:flex-row flex-col md:mt-24 md:me-auto mb-10 mt-12 mt-22 m-2 pt-14">
-          <h1 className="md:text-4xl font-semibold text-3xl md:text-start text-center my-2 md:my-0 md:me-auto">
-            Listado de Productos
-          </h1>
-
-          <div className="flex items-center justify-center md:justify-between md:mx-0 mx-auto">
-            <NavLink
-              to={"#"}
-              className="inline-block  md:mx-0 w-auto md:ms-auto p-2 me-1 md:me-0 bg-red-700 rounded-lg text-lg hover:bg-red-600 text-white"
-            >
-              Agregar Producto
-            </NavLink>
-          </div>
-        </div>
 
         <div className="m-2">
           <AdminListadoProductos grillaShop={grillaShop} />
